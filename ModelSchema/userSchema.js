d861a8898aaada373529b8d405a332a361fcbbf4
mayur-thoughtwin <mayur.panchal@thoughtwin.com>
2025-04-17 11:46:10 +0530

    add validation

--- ModelSchema/userSchema.js
+++ ModelSchema/userSchema.js
@@ -1,10 +1,11 @@
+const { required } = require("joi");
 const mongoose=require("mongoose");
 
 const userSchema=mongoose.Schema({
-    "name":String,
-    "email":String,
-    "mobile_no":Number,
-    "gender":String,
+    "name":{type:String,required:true},
+    "email":{type:String,required:true,unique:true},
+    "mobile_no":{type:Number,required:true,unique:true},
+    "gender":{type:String,required:true},
     "profile":String,
     "file":String
     
